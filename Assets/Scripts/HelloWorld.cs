72c61ee3bee19121ab4d04b876ccbfd6bf17598d
YIN,CHENG-TANG <116491887+yinnick0612@users.noreply.github.com>
2024-03-08 14:43:29 +0800

    FirstComment

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void OnClick()
+    {
+        Debug.Log("Hello World!");
+    }
+}
